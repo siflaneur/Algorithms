2c79991fe92e85335e305e81a085fb6c530813c5
flaneur <e.sif.chen@gmail.com>
2017-11-03 15:47:59 +0800

    Rewrite the code by using 'format(number)'

--- beginner/q01-palindrome.py
+++ beginner/q01-palindrome.py
@@ -2,18 +2,22 @@
 """
 问题：求用十进制、二进制、八进制表示都是回文数的所有数字中，大于十进制数 10 的最小值。
 """
-from utils import reverse
+
+
+def is_palidrome(x, flag='d'):
+    return format(x, flag)[::-1] == format(x, flag)
 
 
 def solution():
     number = 11
     while True:
-        if reverse(str(number)) == str(number) and \
-                        reverse(oct(number)) == oct(number) and \
-                        reverse(bin(number)) == bin(number):
+        if is_palidrome(number) and \
+                is_palidrome(number, 'b') and \
+                is_palidrome(number, 'o'):
             print(number)
             break
-        number += 2  # 二进制数若末尾为0，必然不符合条件
+        # 二进制数若末尾为0，必然不符合条件
+        number += 2
 
 
 if __name__ == '__main__':
